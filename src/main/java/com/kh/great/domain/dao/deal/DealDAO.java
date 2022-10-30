624305a8c1c1bc9d8d042662058b4e070fcc92f9
Junhyeok Heo <wnsgur5508@gmail.com>
2022-10-30 23:08:07 +0900

    + deal DAO

---
+++ src/main/java/com/kh/great/domain/dao/deal/DealDAO.java
@@ -0,0 +1,27 @@
+package com.kh.great.domain.dao.deal;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DealDAO {
+    //구매 등록
+    Deal add(Deal deal);
+
+    //구매 조회 (조회 by 회원번호)
+    List<Deal> findByMemberNumber(Long memNumber);
+
+    //구매 조회 (조회 by 주문번호)
+    Optional<Deal> findByOrderNumber(Long orderNumber);
+
+    //구매 시 판매개수 감소
+    int update(Long pNumber,Deal deal);
+
+    //구매 취소
+    int deleteByOrderNumber(Long orderNumber);
+
+    //구매 취소 시 판매개수 증가
+    int delUpdate(Long pNumber,Deal deal);
+
+    //남은 수량 0개일 시
+    int updatePstatus(Long pNumber);
+}

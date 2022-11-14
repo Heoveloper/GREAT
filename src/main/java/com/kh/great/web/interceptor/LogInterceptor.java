0cbc8a416d74560ca57bd5323c0c774cb866c9d7
Junhyeok Heo <wnsgur5508@gmail.com>
2022-11-14 23:42:23 +0900

    mod Interceptors

--- src/main/java/com/kh/great/web/interceptor/LogInterceptor.java
+++ src/main/java/com/kh/great/web/interceptor/LogInterceptor.java
@@ -1,4 +1,4 @@
-package com.kh.great.web.interceptor.member;
+package com.kh.great.web.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -10,7 +10,6 @@ import java.util.UUID;
 
 @Slf4j
 public class LogInterceptor implements HandlerInterceptor {
-
     //컨트롤러 호출 전
     @Override
     public boolean preHandle(
@@ -18,14 +17,12 @@ public class LogInterceptor implements HandlerInterceptor {
             HttpServletResponse response,
             Object handler
     ) throws Exception {
-        log.info("preHandle called!");
-
         String requestURI = request.getRequestURI(); //클라이언트의 요청 URI
         String uuid = String.valueOf(UUID.randomUUID()); //요청 추적번호
 
         request.setAttribute("transactionId", uuid);
 
-        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+        //log.info("REQUEST [{}] [{}] [{}]", uuid, requestURI, handler);
 
         return true;
     }
@@ -40,16 +37,14 @@ public class LogInterceptor implements HandlerInterceptor {
             Object handler,
             ModelAndView modelAndView
     ) throws Exception {
-        log.info("postHandle called!");
-
         String requestURI = request.getRequestURI(); //클라이언트의 요청 URI
         String uuid = String.valueOf(request.getAttribute("transactionId"));
 
-        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+        //log.info("REQUEST [{}] [{}] [{}]", uuid, requestURI, handler);
     }
 
     //뷰 렌더링 후, 클라이언트에 응답메세지 전송 후
-    //컨트롤러에서 예외발생 유무에 상관없이 항상 호출
+    //컨트롤러에서 예외 발생 유무에 상관없이 항상 호출
     @Override
     public void afterCompletion(
             HttpServletRequest request,
@@ -57,12 +52,10 @@ public class LogInterceptor implements HandlerInterceptor {
             Object handler,
             Exception ex
     ) throws Exception {
-        log.info("afterCompletion!");
-
         String requestURI = request.getRequestURI(); //클라이언트의 요청 URI
         String uuid = String.valueOf(request.getAttribute("transactionId"));
 
-        log.info("REQUEST [{}][{}][{}]", uuid, requestURI, handler);
+        //log.info("REQUEST [{}] [{}] [{}]", uuid, requestURI, handler);
 
         if (ex != null) {
             log.error("afterCompletion error!!", ex);
--- src/main/java/com/kh/great/web/interceptor/LoginInterceptor.java
+++ src/main/java/com/kh/great/web/interceptor/LoginInterceptor.java
@@ -1,4 +1,4 @@
-package com.kh.great.web.interceptor.member;
+package com.kh.great.web.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -10,7 +10,6 @@ import java.net.URLEncoder;
 
 @Slf4j
 public class LoginInterceptor implements HandlerInterceptor {
-
     public boolean preHandle(
             HttpServletRequest request,
             HttpServletResponse response,
@@ -18,21 +17,8 @@ public class LoginInterceptor implements HandlerInterceptor {
     ) throws Exception {
         String redirectUrl = null;
         String requestURI = request.getRequestURI();
-        log.info("인증체크 = {}", requestURI);
 
-        //세션 조회
-//        HttpSession session = request.getSession(false);
-//
-//        if (session == null || session.getAttribute("loginMember") == null) {
-//            log.info("미인증 사용자의 요청");
-//
-//            //로그인 화면으로 redirect
-//            response.sendRedirect("/login?requestURI=" + requestURI);
-//            return false;
-//        }
-//
-//        return true;
-//    }
+        //log.info("인증체크 = {}", requestURI);
 
         if (request.getQueryString() != null) {
             String queryString = URLEncoder.encode(request.getQueryString(), "UTF-8");
@@ -45,13 +31,15 @@ public class LoginInterceptor implements HandlerInterceptor {
             redirectUrl = requestURI;
         }
 
-        //세션정보가 있으면 반환 없으면 null반환
-        HttpSession session = request.getSession(false);
+        //세션정보가 있으면 세션정보(true), 없으면 null(false) 반환
+        HttpSession session = request.getSession(false); //세션 조회
         if (session == null || session.getAttribute("loginMember") == null) {
-            log.info("미인증 요청");
+            log.info("미인증 사용자의 요청!");
+            //response.sendRedirect("/login?requestURI=" + requestURI);
             response.sendRedirect("/login?redirectUrl=" + redirectUrl);
             return false;
         }
+
         return true;
     }
 }

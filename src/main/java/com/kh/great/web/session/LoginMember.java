ad432f58d3ebc2b3208d70260ccdb195afbaaa52
Junhyeok Heo <wnsgur5508@gmail.com>
2022-11-14 23:44:31 +0900

    mod session

--- src/main/java/com/kh/great/web/session/LoginMember.java
+++ src/main/java/com/kh/great/web/session/LoginMember.java
@@ -6,10 +6,15 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class LoginMember {
-    private Long memNumber;
-    private String memType;
-    private String memId;
-    private String memNickname;
-    private String memStoreName;
-    private String memAdmin;
+    private Long memNumber;         //회원번호
+
+    private String memType;         //회원유형
+
+    private String memId;           //아이디
+
+    private String memNickname;     //닉네임
+
+    private String memStoreName;    //가게명
+
+    private String memAdmin;        //관리자여부
 }

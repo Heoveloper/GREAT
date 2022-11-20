e043e32f7daf1529240484a7d73b77d1698efc7d
Junhyeok Heo <wnsgur5508@gmail.com>
2022-11-20 22:24:15 +0900

    mod test files

--- src/test/java/com/kh/great/domain/dao/member/MemberDAOImplTest.java
+++ src/test/java/com/kh/great/domain/dao/member/MemberDAOImplTest.java
@@ -1,20 +1,26 @@
 package com.kh.great.domain.dao.member;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @Slf4j
 @SpringBootTest
 class MemberDAOImplTest {
-
     @Autowired
     private MemberDAO memberDAO;
 
     @Test
+    @DisplayName("아이디 찾기")
+    void findByMemNameAndMemEmail() {
+        Member member = memberDAO.findByMemNameAndMemEmail("허준혁", "test333@test.com");
+        log.info("id = {}", member.getMemId());
+    }
+
+    @Test
+    @DisplayName("비밀번호 재설정")
     void resetPw() {
         Member member = new Member();
         member.setMemPassword("text1234");
